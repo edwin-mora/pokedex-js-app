6699e2f1a6df06719d0a18a256083a05a5571d58
edwinmoraa <58499972+edwinmoraa@users.noreply.github.com>
2022-03-03 14:31:09 -0800

    Update scripts.js
    Created the array pokemonList variable with 3 select pokemon and their stats

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello World');
+let pokemonList = [{name: "Squirtle", height: 0.5, type: ['water']},
+                    {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
+                    {name: 'Pidgey', height: 0.3, type: ['normal', 'flying']}
+];
 
-let favoriteFood = 'tacos';
-document.write(favoriteFood)
