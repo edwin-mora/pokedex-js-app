58c0cf0a96702854c164e53be224921bf32e5f3d
edwinmoraa <58499972+edwinmoraa@users.noreply.github.com>
2022-03-03 14:33:24 -0800

    Update scripts.js
    added comments before to describre what I'm doing

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//creating new pokemonList variable containing 3 pokemons & their stats
+
 let pokemonList = [{name: "Squirtle", height: 0.5, type: ['water']},
                     {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
                     {name: 'Pidgey', height: 0.3, type: ['normal', 'flying']}
