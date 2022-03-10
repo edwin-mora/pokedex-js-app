87ed441294594e193f7b54e823d2ed6c511da701
edwinmoraa <58499972+edwinmoraa@users.noreply.github.com>
2022-03-09 19:31:11 -0800

    Update scripts.js
    created a for loop iterated through the whole array in pokemonList, added conditionals for the height of pokemons as well

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,42 @@
 //creating new pokemonList variable containing 3 pokemons & their stats
 
-let pokemonList = [{name: "Squirtle", height: 0.5, type: ['water']},
-                    {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
-                    {name: 'Pidgey', height: 0.3, type: ['normal', 'flying']}
+let pokemonList = [
+    {name: "Squirtle", height: 0.5, type: ['water']},
+    {name: 'Charizard', height: 1.7, type: ['fire', ' flying']},
+    {name: 'Pidgey', height: 0.3, type: ['normal', ' flying']}
 ];
+// create a for loop that iterates over each item in pokemonList
+let text = "";
+let i = 0;
 
+while (pokemonList[i]){
+    text = text + " " + "<p>" + pokemonList[i].name + " "+  pokemonList[i].height + " " + pokemonList[i].type;
+    i++;
+}
+
+document.write(text);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//create conditionals for the pokemon's height
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 1.0) {
+        document.write('<p>'+ pokemonList[i].name + " " + "(height:" + " " + pokemonList[i].height + ")" + " -whoa that's a big pokemon!")
+    } else if (pokemonList.height > 0.5 && pokemonList.height < 1.5){
+        document.write('<p>' + pokemonList[i].name + " " + "(height:" + " " + pokemonList[i].height + ")" + " -this is an avg pokemon")
+    } else{
+        document.write('<p>' + pokemonList[i].name + " "+ "(height:" + " " + pokemonList[i].height + ")" + " -this is a small pokemon")
+    }
+}
