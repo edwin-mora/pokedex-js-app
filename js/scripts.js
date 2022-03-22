7c15b9e5c42ce2542ea0daee9740e43cd7f4be31
edwinmoraa <58499972+edwinmoraa@users.noreply.github.com>
2022-03-22 16:45:21 -0700

    Update scripts.js
    created IIFE and replaced the for loop used previously to a forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,32 @@
 //creating new pokemonList variable containing 3 pokemons & their stats
 
+//IIFE
+let pokemonRepository = (function (){
+
 let pokemonList = [
     {name: "Squirtle", height: 0.5, type: ['water']},
     {name: 'Charizard', height: 1.7, type: ['fire', ' flying']},
     {name: 'Pidgey', height: 0.3, type: ['normal', ' flying']}
 ];
-// create a for loop that iterates over each item in pokemonList
 
-for (let i = 0; i < pokemonList.length; i++){
-    document.write("<p>" +pokemonList[i].name + "</p>")
-    document.write("<p>" +pokemonList[i].height + "</p>")
-    document.write("<p>" +pokemonList[i].type + "</p>")
+//new public functions
+
+function add (pokemon){
+    pokemonList.push(pokemon);
+}
+function getAll(){
+    return pokemonList;
 }
 
+return {
+    add: add,
+    getAll: getAll
+};
 
+})(); //end IIFE
 
+//create forEach loop
+pokemonRepository.getAll().forEach(function (pokemon){
+    pokemonRepository.add(pokemon)
+});
 
-
-
-
-
-
-
-
-
-
-
-
-//create conditionals for the pokemon's height
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 1.0) {
-        document.write('<p>'+ pokemonList[i].name + " " + "(height:" + " " + pokemonList[i].height + ")" + " -whoa that's a big pokemon!")
-    } else if (pokemonList.height > 0.5 && pokemonList.height < 1.5){
-        document.write('<p>' + pokemonList[i].name + " " + "(height:" + " " + pokemonList[i].height + ")" + " -this is an avg pokemon")
-    } else{
-        document.write('<p>' + pokemonList[i].name + " "+ "(height:" + " " + pokemonList[i].height + ")" + " -this is a small pokemon")
-    }
-}
