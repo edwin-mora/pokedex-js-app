757c010d5353097355f44cbc29f1e3771b6683d4
edwinmoraa <58499972+edwinmoraa@users.noreply.github.com>
2022-03-23 15:31:02 -0700

    Update scripts.js
    added the showDetails function so when pokemon's button is clicked the console logs the name

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,18 @@ let pokemonList = [
 ];
 
 //new public functions
+function add(pokemon) {
+    if (
+        typeof pokemon === 'object' &&
+        'name' in pokemon &&
+        'height' in pokemon &&
+        'type' in pokemon
+    ) {
+        pokemonList.push(pokemon);
+    } else {
+        console.log('pokemon is not correct!');
+    }
+}
 
 function add (pokemon){
     pokemonList.push(pokemon);
@@ -18,15 +30,48 @@ function getAll(){
     return pokemonList;
 }
 
+//shows the pokemon when clicked
+function showDetails (pokemon){
+    console.log(pokemon.name);
+}
+
+//add event listener
+function addListener (button, pokemon) {
+    button.addEventListener ('click', function (){
+        showDetails(pokemon);
+    });
+}
+
+//adds buttons displaying the pokemon's name
+function addListItem(pokemon){
+    let pokemonList = document.querySelector('.pokemon-list');
+    let listPokemon = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('button-class');
+    listPokemon.appendChild(button);
+    pokemonList.appendChild(listPokemon);
+    addListener(button, pokemon);
+}
+
 return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails
 };
 
 })(); //end IIFE
 
-//create forEach loop
-pokemonRepository.getAll().forEach(function (pokemon){
-    pokemonRepository.add(pokemon)
+//add a new pokemon
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({name: 'Pikachu', height: 0.3, type: ['electric']});
+
+console.log(pokemonRepository.getAll());
+
+//call the button function from the IIFE
+pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
 });
+
 
